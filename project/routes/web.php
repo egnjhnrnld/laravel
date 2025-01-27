3d3f5fa144e975f4c1c31d20b402e2b422c2b883
strix8755 <strix8755@gmail.com>
2025-01-27 11:46:56 +0800

    Initial commit

---
+++ project/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\GreetController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return 'Hello, Laravel!';
+});
+
+Route::get('/greet', [GreetController::class, 'show']);
